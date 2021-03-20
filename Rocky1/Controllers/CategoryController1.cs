467196a548dbe9638745d8a939df7a3552d9e9a4
jiin-yang <jinyang.sv.69@gmail.com>
2021-03-20 14:41:19 +0300

    First commit Rocky1

---
+++ Rocky1/Controllers/CategoryController1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky1.Data;
+using Rocky1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky1.Controllers
+{
+    public class CategoryController1 : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController1(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _db.Category;
+            return View(objList);
+        }
+    }
+}
